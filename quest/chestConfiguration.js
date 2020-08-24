26e0fbbdca80793f2b42f58e318bce4f1770bdc5
Roboscorpion <69328645+Roboscorpion@users.noreply.github.com>
2020-08-24 15:25:27 -0400

    Add files via upload

---
+++ quest/chestConfiguration.js
@@ -0,0 +1,7 @@
+const verifiedUser = true;
+const accessLevel = 7;
+const favoriteRobot ='Cedric';
+
+console.log('Is the User verified' + verifiedUser);
+console.log('The accessLevel is' + accessLevel);
+console.log('My favorite robot is' + favoriteRobot);
