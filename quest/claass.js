b938dda575a42e9a49f0f004db6e6d2535fbcafa
Roboscorpion <69328645+Roboscorpion@users.noreply.github.com>
2020-08-26 22:41:54 -0400

    Add files via upload

---
+++ quest/claass.js
@@ -0,0 +1,15 @@
+class Materializer
+    constructor(targetName) {
+function
+}
+
+}
+
+const m = new Materializer('Kevin');
+console.log(m.activated);
+
+
+m.activate();
+console.log(m.activated); // would print "true"
+
+console.log(m.materialize())
